c73c11a872d91037a4e0948bd892509ab84d9e6f
Daianes26 <daianeasilva26@gmail.com>
2019-08-24 00:35:17 -0400

    Finalize game

--- pig-game/app.js
+++ pig-game/app.js
@@ -9,24 +9,136 @@ GAME RULES:
 
 */
 
+let scores;
+let roundScore;
+let activePlayer;
+//state variable
+let gamePlaying;
 
-//declare variables
-const scores = [0,0];
-//array to keep scores for both players
-const roundScore = 0;
-// keep score for each round
-const activePlayer = 1;
-//first player = 0 and second player = 1
+// Initialize game
+init();
 
-const dice = Math.floor(Math.random() * 6) + 1;
+//Start Game Function
+function init(){
+    //declare variables
+    scores = [0,0];
+    //array to keep scores for both players
+    roundScore = 0;
+    // keep score for each round
+    activePlayer = 0;
+    //first player = 0  begins and second player = 1
+    gamePlaying = true;
 
-// query selectors
 
-//set value to the element
-document.querySelector('#current-' + activePlayer).textContent = dice;
+    //hide dice 
+    document.querySelector('.dice').style.display = 'none';
+
+    //Select elements to set score to 0
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    //reset player names
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    //show buttons
+    // document.querySelector('.btn-roll').style.display = 'block';
+    // document.querySelector('.btn-hold').style.display = 'block';
+
+    //reset styles
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+};
+
+//Calls next player
+function nextPlayer(){
+    //ternary operator (if active player === 0 then active player will be 1 and vice-versa)
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    //returns score to 0 if player rolls a 1
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    //Add or remove class active for current player
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //hide dice if player rolls 1
+    document.querySelector('.dice').style.display = 'none';
 
 
-const scoresCount = document.querySelector('#score-' + activePlayer).textContent;
+
+};
+
+
+//Add Event Listener to roll dice button
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    if (gamePlaying){
+            //1. Random number
+        const dice = Math.floor(Math.random() * 6) + 1;
+        //2. Display result
+        const diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src='dice-' + dice + '.png';
+        
+        //3. Update score unless number is a 1.
+        if (dice !== 1){
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+
+        } else {
+            //Next player turn
+            nextPlayer();
+        
+     }
+    }
+  
+
+});
+
+//Add event listener to hold button
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    if (gamePlaying){
+    
+        //Add current score to global score
+        scores[activePlayer] += roundScore;
+
+        //Update user interface
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        //Check if player won the game
+        if (scores[activePlayer] >= 100){
+            //display winner
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
+            // hide dice
+            document.querySelector('.dice').style.display = 'none';
+            //Change style
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    
+            //state variable to end game
+            gamePlaying = false;
+            
+            //hide buttons
+            // document.querySelector('.btn-roll').style.display = 'none';
+            // document.querySelector('.btn-hold').style.display = 'none';
+
+        } else {
+        //Next player turn
+        nextPlayer();
+        }
+    }
+
+
+});
+
+//Add Event Listener to play new game button
+
+document.querySelector('.btn-new').addEventListener('click', init);
 
 
 
